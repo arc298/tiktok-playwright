8d09d35adaadf91a101372632229ddf833f54950
abcbcds <99208550+abcbcds@users.noreply.github.com>
2022-02-07 19:08:23 +0100

    Fix scrollToBottom

--- src/helpers/scrollToBottom.js
+++ src/helpers/scrollToBottom.js
@@ -1,7 +1,7 @@
 const scrollToBottom = async (page, distance = 100, delay = 400) => {
   while (await page.evaluate(() => document.scrollingElement.scrollTop + window.innerHeight < document.scrollingElement.scrollHeight)) {
     await page.evaluate((y) => {
-      document.scrollingElement.scrollBy(0, y)
+      window.scrollBy(0, y)
     }, distance)
 
     await page.waitForTimeout(delay)
